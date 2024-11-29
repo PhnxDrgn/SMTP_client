aa1ee8b30f91586e1bce526b7abd5183c4f89a49
Andy Hoang <andyhoang2010@gmail.com>
2024-11-29 09:36:21 -0500

    wip ssl connection

--- src/main.c
+++ src/main.c
@@ -5,13 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
+#include <stdbool.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/bio.h>
+#include <openssl/evp.h>
 
 #define SMTP_PORT 25
-#define SMTP_TLS_PORT 587
+#define SMTP_SSL_PORT 465
 #define GMAIL_SMTP_NAME "smtp.gmail.com"
 #define MAX_LINE_LEN 100
 #define MAX_USER_LEN 50
 #define MAX_PASS_LEN 50
+#define BUFFER_SIZE 1024
 
 int socketFd = 0;
 char user[MAX_USER_LEN] = "\0";
@@ -178,6 +184,44 @@ int readCredentials(char *filePath)
     return 0;
 }
 
+/**
+ * Function to send command to server
+ * socket: socket descriptor for server
+ * buffer: data to send to server
+ * hideCommand: flag to hide print out of failed command
+ * return: 0 for success or -1 for failure
+ */
+int sendCommand(SSL *ssl, char *cmd, bool hideCommand, int expectedResponse)
+{
+    SSL_write(ssl, cmd, strlen(cmd));
+
+    // read response
+    char buffer[BUFFER_SIZE];
+    int len = SSL_read(ssl, buffer, sizeof(buffer) - 1);
+    buffer[len] = '\0';
+    if (len <= 0)
+    {
+        printf("Failed to read reply to command.\n");
+        return -1;
+    }
+
+    printf("Server response: %s\n", buffer);
+
+    char responseCpy[4];
+    strncpy(responseCpy, buffer, sizeof(responseCpy) - 1);
+    responseCpy[sizeof(responseCpy) - 1] = '\0';
+    int responseCode = atoi(responseCpy);
+
+    if (responseCode != expectedResponse)
+    {
+        printf("Response does not match expected response of %d.\n", expectedResponse);
+        ERR_print_errors_fp(stdout);
+        return -1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     // connect function to handle exits
@@ -213,6 +257,21 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    // Initialize OpenSSL
+    printf("Initializing OpenSSL\n");
+    SSL_CTX *ctx;
+    SSL *ssl;
+    BIO *bio;
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    const SSL_METHOD *method = SSLv23_client_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx)
+    {
+        ERR_print_errors_fp(stdout);
+        exit(EXIT_FAILURE);
+    }
+
     // creating socket
     socketFd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketFd < 0)
@@ -235,17 +294,41 @@ int main(int argc, char *argv[])
     struct sockaddr_in serverAddress;
     memset(&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(SMTP_TLS_PORT);
+    serverAddress.sin_port = htons(SMTP_SSL_PORT);
     memcpy(&serverAddress.sin_addr.s_addr, host->h_addr_list[0], host->h_length);
 
     // connect to server
     if (connect(socketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
     {
         printf("Failed to connect to server.\n");
-        closeSocket();
         exit(EXIT_FAILURE);
     }
     printf("Successfully connected to server\n");
+
+    // Create SSL object
+    printf("Creating SSL object\n");
+    bio = BIO_new_socket(socketFd, BIO_NOCLOSE);
+    ssl = SSL_new(ctx);
+    SSL_set_bio(ssl, bio, bio);
+
+    // Perform the handshake
+    if (SSL_connect(ssl) <= 0)
+    {
+        ERR_print_errors_fp(stdout);
+        exit(EXIT_FAILURE);
+    }
+
+    // read server response. Expecting 220 for server ready
+    if (sendCommand(ssl, "", false, 220) < 0)
+        exit(EXIT_FAILURE);
+
+    // send EHLO to introduce and set domain. Expecting 250 for auth request accepted
+    if (sendCommand(ssl, "EHLO umd.edu\r\n", false, 250) < 0)
+        exit(EXIT_FAILURE);
+
+    // request auth login. Expecting 334 for auth request accepted
+    if (sendCommand(ssl, "AUTH LOGIN\r\n", false, 334) < 0)
+        exit(EXIT_FAILURE);
 
     exit(EXIT_SUCCESS);
 }
